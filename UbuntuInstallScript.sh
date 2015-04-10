d45ca4b31662d9f7ae3c6a310ec18edeee24d56f
v3nturetheworld <pheinenjr@gmail.com>
2015-04-10 13:18:44 -0600

    Readme updated. Install Script added (but not tested). Some other files modified

---
+++ UbuntuInstallScript.sh
@@ -0,0 +1,76 @@
+#!/bin/bash
+#DU Printers Install tool
+#Written by Paul Heinen
+
+wget=/usr/bin/wget
+tar=/bin/tar
+#Store the location of this script
+DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+PyInstall=false
+
+echo "DU Printer Installation Tool for Debian Based Linux (Yes that includes any form of Ubuntu)."
+echo "Please note that this script hasn't been thoroughly tested. Hopefully it just works."
+if ["$UID" -ne "$ROOT_UID"]
+then
+	echo "You to be root to run this script"
+	exit
+else
+	
+	read -p "Script ran as root. Do you want to continue with the installation? (y/n) " RESP
+	if["$RESP" = "y" ]; then
+		echo "Checking Python Version"
+		ret=`python -c 'import sys; print("%i" % (sys.hexversion<0x03000000))'`
+		if [ $ret -eq 0 ]; then
+        PyInstall=true
+        if [ sudo apt-get install python2.7 -y]; then
+          echo "Python2.7 was succesfully installed using your package manager"
+        else
+          echo "Your package manager failed to install python. Attempting to download an build package"
+   			  echo "Python detected is not 2.7. Installing Python 2.7."
+   			  echo -e "\nSetting up the Install Environment. This will take some time..."
+        	sudo apt-get install zlib* zlib-devel readline-devel  openssl-devel bzip2-devel wget gcc python-dev -y
+       
+      	  	# Compile and Build Python 2.7 under /opt
+        	cd /usr/local/src/
+        	echo "Assuming you have wget pre-installed."
+        	wget http://www.python.org/ftp/python/2.7.1/Python-2.7.1.tgz
+        	tar xvfz Python-2.7.1.tgz
+        	cd Python-2.7.1
+        	./configure --prefix=/opt/python2.7.1 --with-threads --enable-shared
+        	make
+        	make install
+        	# for shared libraries
+       		touch /etc/ld.so.conf.d/opt-python2.7.1.conf
+       		echo "/opt/python2.7.1/lib/" >> /etc/ld.so.conf.d/opt-python2.7.1.conf
+       		ldconfig
+        	ln -sf /opt/python2.7.1/bin/python2 /usr/bin/python2.7
+        	echo "Python2.7 Install was succesful!"
+        fi
+		fi
+    
+    # Python2.7 is already be installed
+    echo "You have a valid version of Python installed. Installing WxPython."
+    if [sudo apt-get install python-wxgtk2.8 -y]; then
+      echo "WxPython was installed succesfully."
+    else
+      echo "WxPython failed to install! Install will proceed WxPython is required to print"
+    fi
+    
+    echo "Downloading and Installing HP Drivers."
+    # Compile and Build Python 2.7 under /opt
+		if [sudo apt-get install --assume-yes avahi-utils libcups2 cups libcups2-dev cups-bsd cups-client libcupsimage2-dev libdbus-1-dev build-essential ghostscript openssl libjpeg-dev libsnmp-dev libtool libusb-1.0.0-dev wget python-imaging policykit-1 policykit-1-gnome gtk2-engines-pixbuf python-qt4 python-qt4-dbus python-dbus python-gobject python-dev python-notify2 python python-reportlab libsane libsane-dev sane-utils xsane zip]; then
+      echo "Printer dependencies installed."
+      if [sudo apt-get install hiplip -y]; then
+        echo "hiplip succesfully installed."
+        cd DIR
+        echo "Hopefully everything was installed correctly. Attempting pharos setup"
+        if ["$PyInstall" = false]; then
+          sudo python setup.py
+        else
+          sudo python2.7 setup.py
+        fi
+      fi
+    fi
+	
+fi
+
